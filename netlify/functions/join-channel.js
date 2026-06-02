37073446fb8cc82b697de7137be125ac3c360228
Annor Gyamera Jesse <111619190+Annor-Gyamera-Jesse@users.noreply.github.com>
2026-06-02 11:38:15 +0000

    added it back

--- netlify/functions/join-channel.js
+++ netlify/functions/join-channel.js
@@ -1,5 +1,5 @@
-/**in future maybe i will do the manual thing lol
- *  ============================================================
+/**
+ * ============================================================
  * SOPHIE FX — join-channel.js
  * Netlify Function: GET /.netlify/functions/join-channel
  *
